ff79df9688873a0fe4db0dbf1768289afa0ce464
sanyawerner <suicide.sheep.sp@gmail.com>
2021-02-17 10:51:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/firstActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class firstActivity extends Fragment
+
+{
+    @Nullable
+    @Override
+    public View onCreateView (@Nullable LayoutInflater inflater, @NonNull ViewGroup container,
+                              @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.first_main,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.myapplication.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.myapplication.R;
+import com.example.myapplication.firstActivity;
+import com.example.myapplication.secondActivity;
+import com.example.myapplication.thirdActivity;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+     switch (position)
+     {
+         case 0:
+             firstActivity Tab1 = new firstActivity();
+             return Tab1;
+         case 1:
+             secondActivity Tab2 = new secondActivity();
+             return Tab2;
+         case 02:
+             thirdActivity Tab3 = new thirdActivity();
+             return Tab3;
+     }
+     return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
